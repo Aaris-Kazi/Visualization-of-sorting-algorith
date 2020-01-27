45b854e6ea641cf63be7d3c32cc3e2fe5d90ac1b
Aaris Kazi <40735215+Aaris-Kazi@users.noreply.github.com>
2020-01-27 21:25:49 +0530

    It is done by Bubble sorting algorithm

---
+++ ALGO.C
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+int main()
+{
+	int x1,y1,x2,y2,n,i,j,temp;
+	int a[50];
+	int gd=DETECT,gm;
+	printf("Enter the number of lines:");
+	scanf("%d",&n);
+	printf("Enter the value:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	clrscr();
+	initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+	line(0,450,0,0);
+	line(550,450,0,450);
+
+	for ( i = 0; i < n-1; i++)
+	{
+		for ( j = 0; j < n-i-1; j++)
+		{
+			if (a[j] > a[j+1])
+			{
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+
+
+	
+	for(i =0,j=5; i<n; i++,j +=20)
+	{
+		line(20+j,450,20+j,a[i]);
+	}
+	getch();
+	closegraph();
+	return 0;
+}
