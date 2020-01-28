b2caf2b5343cae68f45048a5ff9c0e53b74ec2cb
Aaris Kazi <40735215+Aaris-Kazi@users.noreply.github.com>
2020-01-28 18:44:13 +0530

    Add files via upload

--- ALGO.C
+++ ALGO.C
@@ -5,6 +5,7 @@ int main()
 {
 	int x1,y1,x2,y2,n,i,j,temp,k;
 	int a[50];
+	char output[256];
 	int gd=DETECT,gm;
 	printf("Enter the number of lines:");
 	scanf("%d",&n);
@@ -18,7 +19,8 @@ int main()
 	setcolor(2);
 	line(0,450,0,0);
 	line(550,450,0,450);
-	for ( i = 0; i < n-1; i++)
+	setcolor(15);
+	for ( i = 0; i < n; i++)
 	{
 		for ( j = 0; j < n-i-1; j++)
 		{
@@ -31,13 +33,14 @@ int main()
 			}
 		}
 	}
-	setcolor(15);
-	for(i=0,j=5;i<n;i++,j +=5)
+	for(i=0,j=5;i<n;i++,j +=20)
 	{
 		line(20+j,450,20+j,a[i]);
+		sprintf(output,"%d",a[i]);
+		outtextxy(20+j,450,output);
 		sleep(1);
 	}
 	getch();
 	closegraph();
 	return 0;
-}
+ }
